e9d99adbbbd3fd417a49e383b82d58d4469cc361
drelbszoomer <173870110+drelbszoomer@users.noreply.github.com>
2026-01-17 19:17:27 -0500

    Add releasegen

--- build_scripts/final_clean.sh
+++ build_scripts/final_clean.sh
@@ -6,9 +6,7 @@ echo "::group::Executing final_clean"
 trap 'echo "::endgroup::"' EXIT
 
 # Clean up /boot
-RUN rm -rf /boot/*
+rm -rf /boot/*
 
 # Clean up /var state that shouldn't be in the image
-RUN rm -rf /var/lib/dnf/repos/* \
-    && rm -f /var/lib/freeipmi/ipckey \
-    && find /var -type f -name 'countme' -delete
+rm -rf /var/*
